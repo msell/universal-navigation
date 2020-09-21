7d89f9cef1825a27ee629e52f9d87798ef08dee1
Matt <matt.sell@beacon.works>
2020-09-21 11:40:55 -0500

    add signout to mech stack

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Button, View, Text, StyleSheet } from "react-native";
+import { AuthContext } from "../AuthContext";
 
 export const SettingsScreen: React.FC = () => {
+  const { dispatch } = React.useContext(AuthContext);
   return (
     <View style={styles.container}>
       <Text>Settings</Text>
+      <View style={styles.signout}>
+        <Button
+          title="Sign Out"
+          onPress={() => dispatch!({ type: "REQUEST_SIGN_OUT" })}
+        />
+      </View>
     </View>
   );
 };
@@ -12,5 +20,9 @@ export const SettingsScreen: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  signout: {
+    marginTop: 30,
+    width: 100,
   },
 });
